44e9bccb2a23a825ac8a68327ab73597725b9289
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-02-14 08:36:46 +0000

    14feb25ProductOfTheLastKNumbers

--- Solution.java
+++ Solution.java
@@ -5,18 +5,31 @@ import java.util.List;
 import java.util.Set;
 
 class ProductOfNumbers {
-
+    List<Integer> proIntegers;
+    int zeroIndex;
     public ProductOfNumbers() {
-        
+        proIntegers = new ArrayList<Integer>();
+        zeroIndex = -1;
     }
     
     public void add(int num) {
-        
+        if(proIntegers.size() == 0 || proIntegers.get(proIntegers.size()-1) == 0){
+            proIntegers.add(num);
+        }
+        else{
+            proIntegers.add(proIntegers.get(proIntegers.size()-1) * num);
+        }
+        if(num == 0){
+            zeroIndex = proIntegers.size()-1;
+        }
     }
     
     public int getProduct(int k) {
-        
+        int n = nums.size();
+        if(proIntegers.size() == 0 ||  n-k>= zeroIndex) return 0;
+        return proIntegers.get(proIntegers.size()-1);
     }
+
 }
 
 /**
