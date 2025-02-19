e304068e2a1c17417b2c2a08ea0a7a6c463a4124
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-02-19 12:17:15 +0000

    merge intervals

--- Solution.cpp
+++ Solution.cpp
@@ -1,41 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 class Solution {
-
-public:
-    void helper(string &currword, string &endWord, unordered_set<string>& wordList, vector<string> currseq, vector<vector<string>> result){
-        if(currword == endWord ){
-            currseq.push_back(currword);
-            result.push_back(currseq);
-            return;
+    public:
+    string getHappyString(int n, int k) {
+        if(n==0) return "";
+        int totalpossible = 3*(1<<n-1);
+        if(totalpossible < k) return "";
+        string ans = "";
+        k--;
+        string s = "";
+        int temp = k / (1<<n-1);
+        k = k % (1<<n-1);
+        s = 'a' + temp;
+        for(int i=1; i<n; i++){
+            temp = k / (1<<n-1-i);
+            k = k % (1<<n-i-1);
+            temp += (s[i-1] - 'a') + 1;
+            temp %= 3;
+            s += 'a' + temp;
         }
-        if(wordList.find(currword) != wordList.end()){
-            return;
-        }
-        wordList.erase(currword);
-        currseq.push_back(currword);
-        for(int i = 0; i<currword.length(); i++){
-            int currChar = currword[i];
-            for(char c = 'a'; c <= 'z'; c++){
-                if(currChar == c) continue;
-                currword[i] = c;
-                helper(currword, endWord, wordList, currseq, result);
-            }
-        }
-        wordList.insert(currword);
-        currseq.pop_back();
-        return;;
-    }
-    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        if(beginWord == endWord) return {{beginWord, endWord}};
-        unordered_set<string> words;
-        for(auto itr: wordList){
-            words.insert(itr);
-        }
-        words.insert(beginWord);
-        words.insert(endWord);
-
+        return s;
     }
 };
--- Solution.java
+++ Solution.java
@@ -6,37 +6,22 @@ import java.util.Set;
 
 
 class Solution {
-
-    void helper(int x, char[] original, StringBuilder temp, Integer result, boolean[] visited) {
-        if(x == 0){
-            
-            return;
-        }
-        for (int i = 0; i < original.length; i++) {
-            if((i==0 || original[i-1] != original[i] || visited[i-1] ) && !visited[i] ){
-                visited[i] = true;
-                helper(x - 1, original, temp, result, visited);
-                visited[i] = false;
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        int l = 0;
+        Arrays.sort(intervals, (int[] a, int[]b)-> a[0]-b[0]);
+        for(int i=1; i<n; i++){
+            if(intervals[l][1] < intervals[i][0]){
+                intervals[++l] = intervals[i];
+            }
+            else{
+                intervals[l][1] = Math.max(intervals[i][1], intervals[l][1]);
             }
         }
-    }
-   
-    public int numTilePossibilities(String tiles) {
-    
-        char[] original = tiles.toCharArray();
-        int n = original.length;
-        Integer result = new Integer();
-        result = 0;
-        Arrays.sort(original);
-
-        for (int i = 1; i < original.length + 1; i++) {
-            Set<String> set = new HashSet<String>();
-            boolean[] visited = new boolean[n];
-             helper(i, original, new StringBuilder(), result, visited);
-             //System.out.println(set.size());
-             result += set.size();
+        int[][] result = new int[l+1][2];
+        for(int i=0; i<=l; i++){
+            result[i] = intervals[i];
         }
         return result;
-      
     }
 }
