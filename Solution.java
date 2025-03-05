d4459f0381017fabbbe88945d0a3efa555c024e9
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-03-05 15:01:29 +0000

    Mathematical solution for the problem

--- Solution.java
+++ Solution.java
@@ -8,17 +8,7 @@ import java.util.Stack;
 
 class Solution {
     public long coloredCells(int n) {
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = 4*i -4;
-        }
-        
-        long res = 0;
-        for(int i = 1; i <= n; i++){
-            res += dp[i];
-        }
-        return res;
+        return 1+(long)2*n*(n-1);
     }
 }
 
