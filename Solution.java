20f158812e3881ea7e55c3e78c3e4411ce4b2eed
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-03-05 14:59:56 +0000

    Count Total Number of Colored Cells

--- Solution.java
+++ Solution.java
@@ -7,13 +7,22 @@ import java.util.Set;
 import java.util.Stack;
 
 class Solution {
-    public boolean checkPowersOfThree(int n) {
-        while(n!=0){
-            if(n%3==2){
-                return false;
-            }
-            n=n/3;
+    public long coloredCells(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = 4*i -4;
         }
-        return true;
+        
+        long res = 0;
+        for(int i = 1; i <= n; i++){
+            res += dp[i];
+        }
+        return res;
     }
 }
+
+// 1 -> 1
+// 2 -> 4 (2+2+ 0+ 0)
+// 3 -> 8 ( 3+ 3+ 1+ 1)
+// 4 -> 12 ( 4+ 4+ 2+ 2)
