6f45f3a6e407f6b0f257290e9596a779a01cae78
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-03-11 20:09:30 +0000

    Number of Substrings Containing All Three Characters

--- Solution.java
+++ Solution.java
@@ -2,34 +2,23 @@ import java.sql.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
-
-
 class Solution {
-    public int[] closestPrimes(int left, int right) {
-        boolean[] primes = new boolean[right+1];
-        Arrays.fill(primes, true);
-        int prev = 0;
-        int res = Integer.MAX_VALUE;
-        int[] ret= new int[]{-1,-1};
-        for(int i=2; i<=right; i++){
-            if(primes[i]) {
-                for(int j=2*i; j<=right; j+=i){
-                    primes[j] = false;
-                }
-                if(i < left) continue;
-                if(res > i-prev && prev!=0) {
-                    res = i-prev;
-                    ret = new int[]{prev, i};
-                }
-                prev = i;
-                continue;
-            }
-            
+
+    public int numberOfSubstrings(String s) {
+        int len = s.length();
+        int[] lastPos = { -1, -1, -1 };
+        int total = 0;
+
+        for (int pos = 0; pos < len; pos++) {
+            lastPos[s.charAt(pos) - 'a'] = pos;           
+            total += 1 + Math.min(lastPos[0], Math.min(lastPos[1], lastPos[2]));
         }
-        return ret;
+
+        return total;
     }
 }
