e6f1f5ba9303c50198f7ceee1433ba70667ad5fb
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-03-12 17:49:09 +0000

    Maximum Count of Positive Integer and Negative Integer

--- Solution.java
+++ Solution.java
@@ -6,19 +6,19 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
-
 class Solution {
 
-    public int numberOfSubstrings(String s) {
-        int len = s.length();
-        int[] lastPos = { -1, -1, -1 };
-        int total = 0;
+    public int maximumCount(int[] nums) {
+        int positiveCount = 0, negativeCount = 0;
 
-        for (int pos = 0; pos < len; pos++) {
-            lastPos[s.charAt(pos) - 'a'] = pos;           
-            total += 1 + Math.min(lastPos[0], Math.min(lastPos[1], lastPos[2]));
+        for (int num : nums) {
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            }
         }
 
-        return total;
+        return Math.max(positiveCount, negativeCount);
     }
 }
