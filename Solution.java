6b6205fe30368ef0587119044303f84ce2e43de8
vikashnitwkumar <32215833+vikashnitwkumar@users.noreply.github.com>
2025-03-13 20:55:35 +0000

    Zero Array Transformation II

--- Solution.java
+++ Solution.java
@@ -6,19 +6,38 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
+
+
 class Solution {
-
-    public int maximumCount(int[] nums) {
-        int positiveCount = 0, negativeCount = 0;
-
-        for (int num : nums) {
-            if (num > 0) {
-                positiveCount++;
-            } else if (num < 0) {
-                negativeCount++;
+    boolean helper(int[] nums, int[][] queries, int qn){
+        int n = nums.length;
+        int[] prefix = new int[n + 1];
+       for (int i = 0; i <= qn; i++) {
+            prefix[queries[i][0]] += queries[i][2];
+            prefix[queries[i][1] + 1] -= queries[i][2];
+       }
+       int temp = 0;
+       for (int i = 0; i < n; i++) {
+           temp += prefix[i];
+           if(temp <nums[i]) return false;
+       }
+        return true;
+    }
+    public int minZeroArray(int[] nums, int[][] queries) {        
+        int n = queries.length;
+        int l = 0, r = n;
+        int result = -2;
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        if(maxVal <= 0) return 0;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (helper(nums, queries, mid)) {
+                result = mid;
+                r = mid;
+            } else {
+                l = mid + 1;
             }
         }
-
-        return Math.max(positiveCount, negativeCount);
+        return result+1;
     }
 }
